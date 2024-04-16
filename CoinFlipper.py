df065ce1259f6b932848a9388b0a9e51ab2318d5
Connor <126129332+CaVarney3@users.noreply.github.com>
2024-04-15 22:39:53 -0400

    Update CoinFlipper.py

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -1,31 +1,24 @@
-import coin
+import random
+# The Coin Flip itself and the Choices
+def coin_flip():
+# Variables
+  number= input("How many times would you like to flip the Coin: ")
+  heads=[]
+  tails=[]
+  recordlist=[]
+# Coin Flip Generation  
+  for amount in range(number):
+    flip = random.randint(0,1)
+    if (flip == 1):
+      print("You flipped the Coin and it landed on Heads!"
+      recordlist.append("Heads")
+    Else:
+      print("You flipped the coin and it landed on Tails!"
+      recordlist.append("Tails")
+# Recording Coin Flip stats
+  print(str(recordList))
+  print(str(recordList.count("Heads")) + str(recordList.count("Tails")))
 
-def(main):
-  # 3 Coin Selection.
-  coin1 = coin.Coin() 
-  coin2 = coin.Coin() 
-  coin3 = coin.Coin() 
-  # Display the side of each coin that is facing up.
 
-  print ('I have three coins with these sides up: ') 
-  print (coin1.get_sideup())
-  print (coin2.get_sideup()) 
-  print (coin3.get_sideup()) 
-  print ()
-  
-   # Toss the coin. 
-  print ('I am tossing all three coins...') 
-  print () 
-  coin1.toss() 
-  coin2.toss() 
-  coin3.toss() 
 
-  # Display Results. print ('Now here are the sides that are up: ') 
-  print (coin1.get_sideup())
-  print (coin2.get_sideup()) 
-  print (coin3.get_sideup()) 
-  print ()
-  
-# Call the main function. 
-if __name__ == '__main__':
-    main()
+
