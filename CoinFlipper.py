f196996395c4fe668b6bcd50ee041f9ebc3eed7b
Connor <126129332+CaVarney3@users.noreply.github.com>
2024-04-15 22:43:23 -0400

    Update CoinFlipper.py
    Fixed Various Issues, Now will work.

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -1,24 +1,23 @@
 import random
-# The Coin Flip itself and the Choices
+
 def coin_flip():
-# Variables
-  number= input("How many times would you like to flip the Coin: ")
-  heads=[]
-  tails=[]
-  recordlist=[]
-# Coin Flip Generation  
-  for amount in range(number):
-    flip = random.randint(0,1)
-    if (flip == 1):
-      print("You flipped the Coin and it landed on Heads!")
-      recordlist.append("Heads")
-    Else:
-      print("You flipped the coin and it landed on Tails!")
-      recordlist.append("Tails")
-# Recording Coin Flip stats
-  print(str(recordList))
-  print(str(recordList.count("Heads")) + str(recordList.count("Tails")))
+    number = int(input("How many times would you like to flip the Coin: "))
+    heads = []
+    tails = []
+    recordlist = []
 
+    for amount in range(number):
+        flip = random.randint(0, 1)
+        if flip == 1:
+            print("You flipped the Coin and it landed on Heads!")
+            recordlist.append("Heads")
+        else:
+            print("You flipped the coin and it landed on Tails!")
+            recordlist.append("Tails")
 
+    print(str(recordlist))
+    print("Heads:", recordlist.count("Heads"))
+    print("Tails:", recordlist.count("Tails"))
 
+coin_flip()
 
