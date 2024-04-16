f04ad70ecb191b6989b56f12f0c2e61f13cbfcc0
Connor <126129332+CaVarney3@users.noreply.github.com>
2024-04-15 22:41:11 -0400

    Update CoinFlipper.py
    Forgot Parenthesis

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -10,10 +10,10 @@ def coin_flip():
   for amount in range(number):
     flip = random.randint(0,1)
     if (flip == 1):
-      print("You flipped the Coin and it landed on Heads!"
+      print("You flipped the Coin and it landed on Heads!")
       recordlist.append("Heads")
     Else:
-      print("You flipped the coin and it landed on Tails!"
+      print("You flipped the coin and it landed on Tails!")
       recordlist.append("Tails")
 # Recording Coin Flip stats
   print(str(recordList))
